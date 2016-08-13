192aa4409fcafe54fefd554881bb8bdb004bc139
Gael Arrambide <gael_arrambide@hotmail.com>
2016-08-13 10:15:26 -0500

    hint and winner picture display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,11 @@ window.onload = function () {
 	var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
-	var randWord, wordChoosen, hint, attempts, letterGuess, letterUsed=[], counter, userChoice;
+	var randWord, wordChoosen, hint, attempts, letterGuess, letterUsed=[], counter, wrongLetter=[];
 	var showAttempts=document.getElementById("attempts");
 	var showHint=document.getElementById("hint");
-	var showGuessLetters=document.getElementById("letterGuess");
-	var showClue = document.getElementById("clue");
-	
+	var showClue=document.getElementById("clue");
+
 	result = function () {
     	wordHolder = document.getElementById('showWord');
     	correct = document.createElement('ul');
@@ -16,7 +15,7 @@ window.onload = function () {
     		correct.setAttribute('id', 'my-word');
       		letterGuess = document.createElement('li');
         	letterGuess.innerHTML = "_";
-
+        	
       		letterUsed.push(letterGuess);
       		wordHolder.appendChild(correct);
       		correct.appendChild(letterGuess);
@@ -31,55 +30,60 @@ window.onload = function () {
     for (var i = 0; i < letterUsed.length; i++) {
       if (counter=== letterUsed.length) {
         showAttempts.innerHTML = "You Win!";
+        youWin();
       }
     }
-		}	
-	
+		}
+
+	youWin=function(){
+		winnerPic=["bulbasaur.png", "squirtle.png", "charmander.png", "pikachu.png"];
+		var winnerIndex= randWord.indexOf(wordChoosen);
+		showClue.innerHTML="<img src='assets/images/" + winnerPic[winnerIndex] + "' class='pokemonimg'>";
+	}
 
 	check = function () {
 		document.onkeyup=function (event){
       	var guess = String.fromCharCode(event.keyCode).toLowerCase();
-      	event.onkeyup=null;
       	for (var i = 0; i < wordChoosen.length; i++) {
-        if (wordChoosen[i] === guess) {
-          letterUsed[i].innerHTML = guess;
-          counter += 1;
-        } 
-      }
-      	var j = (wordChoosen.indexOf(guess));
-      	if (j === -1) {
-        attempts -= 1;
-        comments();
-      	} else {
-        	comments();
-      	}
-	}
+        	if (wordChoosen[i] === guess) {
+          		letterUsed[i].innerHTML = guess;
+          		counter += 1;
+        		}
+        	}
+      		var j = (wordChoosen.indexOf(guess));
+      		if (j === -1) {
+        		attempts -=1;
+        		comments();
+      			} else {
+        		comments();
+      		}
+		}
 	}
 
+	//Start playing
 	start=function(){
-	//alert("You just begin playing");
 	randWord=["bulbasaur", "squirtle", "charmander", "pikachu"];
 	wordChoosen=randWord[Math.floor(Math.random()*randWord.length)];
 	wordChoosen=wordChoosen.replace(/\s/g, "-");
 	console.log(wordChoosen);
 	check();
 	letterUsed=[];
-	attempts=10;
+	attempts=5;
 	counter=0;
 	result();
 	comments();
 		}
 
-	hint.onclick=function(){
-
-		hints=["../images/bulbasaur.png", "../images/squirtle.png", "../images/charmander.png", "../images/pikachu.png"];
+	//shows hint after click on button hint
+	showHint.onclick=function(){
+		hints=["bulbasaur1.png", "squirtle1.png", "charmander1.png", "pikachu1.png"];
 		var hintIndex= randWord.indexOf(wordChoosen);
-		showClue.innerHTML=hints[hintIndex];
+		showClue.innerHTML="<img src='assets/images/" + hints[hintIndex] + "' class='pokemonimg'>";
 	}
 
-	reset=function(){
+	//reset before continue playing again
+	reset.onclick=function(){
 		correct.parentNode.removeChild(correct);
-    	letters.parentNode.removeChild(letters);
     	showClue.innerHTML = "";
 		start();
 	}
